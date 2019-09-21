df7e59c69629e872aee92b8694cef961a38f3b80
Fumiaki Kinoshita <fumiexcel@gmail.com>
2019-09-21 20:30:11 +0900

    api demo

--- wai-middleware-webauthn/demo/Main.hs
+++ wai-middleware-webauthn/demo/Main.hs
@@ -10,6 +10,7 @@ import Network.HTTP.Types
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Yaml as Yaml
+import qualified Data.Aeson as J
 import Paths_demo
 
 main :: IO ()
@@ -20,4 +21,9 @@ main = do
   pathCert <- getDataFileName "certificate.pem"
   pathKey <- getDataFileName "key.pem"
   runTLS (tlsSettings pathCert pathKey) (setPort 8080 defaultSettings) { settingsHTTP2Enabled = False }
-    $ mid $ \req sendResp -> sendResp $ responseFile status200 [] path Nothing
+    $ mid $ \req sendResp -> case pathInfo req of
+      [] -> sendResp $ responseFile status200 [] path Nothing
+      ["api"] -> case WebAuthn.requestIdentifier req of
+        Nothing -> sendResp $ responseLBS status401 [] "Authorisation required"
+        Just name -> sendResp $ responseLBS status200 [] $ J.encode name
+      _ -> sendResp $ responseLBS status404 [] "Not found"
