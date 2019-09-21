cc0014afda8824349ae57beea78ef6ff92de0873
Fumiaki Kinoshita <fumiexcel@gmail.com>
2019-09-21 21:58:33 +0900

    promise

--- wai-middleware-webauthn/lib.js
+++ wai-middleware-webauthn/lib.js
@@ -1,42 +1,46 @@
 function WebAuthnProxy(endpoint){
   result = {};
 
-  function getJSON(path, success, failure)
+  function getJSON(path)
   {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          success(JSON.parse(xhr.responseText));
-        } else {
-          failure(xhr.responseText);
+    return new Promise(function(resolve, reject){
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function()
+      {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.responseText));
+          } else {
+            reject(xhr.responseText);
+          }
         }
-      }
-    };
-    xhr.open("GET", path, true);
-    xhr.send();
+      };
+      xhr.open("GET", path, true);
+      xhr.send();
+    });
   }
 
-  function postJSON(path, body, success, failure)
+  function postJSON(path, body)
   {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          success(JSON.parse(xhr.responseText));
-        } else {
-          failure(xhr.responseText);
+    return new Promise(function(resolve, reject){
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function()
+      {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            resolve(JSON.parse(xhr.responseText));
+          } else {
+            reject(xhr.responseText);
+          }
         }
-      }
-    };
-    xhr.open("POST", path, true);
-    xhr.send(body);
+      };
+      xhr.open("POST", path, true);
+      xhr.send(body);
+    });
   }
 
-  result.register = function (user, cont){
-    getJSON(endpoint + "/challenge", function(challenge){
+  result.register = user => new Promise(function(resolve, reject){
+    getJSON(endpoint + "/challenge").then(function(challenge){
       let rawChallenge = base64js.toByteArray(challenge);
       let info =
           { challenge: rawChallenge
@@ -54,20 +58,15 @@ function WebAuthnProxy(endpoint){
             , CBOR.encode(
                 [ new Uint8Array(cred.response.clientDataJSON)
                 , new Uint8Array(cred.response.attestationObject)
-                , new Uint8Array(rawChallenge)])
-            , cont);
+                , new Uint8Array(rawChallenge)])).then(resolve).catch(reject);
         })
-        .catch((err) => {
-          console.log("ERROR", err);
-        });
-    });
-  };
+        .catch((err) => reject(err));
+    })});
 
-  result.lookup = function(name, cont){
-    getJSON(endpoint + "/lookup/" + name, cont)
-  }
-  result.verify = function(credStr, cont){
-    getJSON(endpoint + "/challenge", function(challenge){
+  result.lookup = name => getJSON(endpoint + "/lookup/" + name);
+
+  result.verify = credStr => new Promise(function(resolve, reject){
+    getJSON(endpoint + "/challenge").then(function(challenge){
       let rawChallenge = base64js.toByteArray(challenge);
       let credId = base64js.toByteArray((credStr + '==='.slice((credStr.length + 3) % 4))
           .replace(/-/g, '+')
@@ -85,13 +84,9 @@ function WebAuthnProxy(endpoint){
                 , new Uint8Array(cred.response.clientDataJSON)
                 , new Uint8Array(cred.response.authenticatorData)
                 , new Uint8Array(cred.response.signature)
-                , new Uint8Array(rawChallenge)])
-            , cont);
+                , new Uint8Array(rawChallenge)])).then(resolve).catch(reject);
         })
-        .catch((err) => {
-          console.log("ERROR", err);
-        });
-    });
-  }
+        .catch(reject);
+    })});
   return result;
 }
