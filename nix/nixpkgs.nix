961647cea653c2eab1766edc8a443005eff01fba
Maciej Kazulak <kazulakm@gmail.com>
2021-04-15 21:34:06 +0200

    Reorganize, make it build with nixos 20.09 stable

---
+++ nix/nixpkgs.nix
@@ -0,0 +1,6 @@
+import (builtins.fetchTarball {
+  name = "nixos-20.09-2021-02-15";
+  url = "https://github.com/NixOS/nixpkgs/archive/2118cf551b9944cfdb929b8ea03556f097dd0381.tar.gz";
+  sha256 = "0ajsxh1clbf3q643gi8v6b0i0nn358hak0f265j7c1lrsbxyw457";
+})
+
